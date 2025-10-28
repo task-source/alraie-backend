398493ec221f5cad13155d5f5126cfbbb5e93936
Yash Jain <yash@belgiumwebnet.com>
2025-10-28 23:04:21 +0530

    Tried fixing message language not rendering

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,17 +1,17 @@
 import i18next from 'i18next';
-import Backend from 'i18next-fs-backend';
 import middleware from 'i18next-http-middleware';
-import path from 'path';
+import en from '../locales/en.json';
+import ar from '../locales/ar.json';
 
 export const initI18n = async () => {
   await i18next
-    .use(Backend)
     .use(middleware.LanguageDetector)
     .init({
       fallbackLng: 'en',
       preload: ['en', 'ar'], // preload both languages
-      backend: {
-        loadPath: path.join(__dirname, '../locales/{{lng}}.json'),
+      resources: {
+        en: { translation: en },
+        ar: { translation: ar },
       },
       detection: {
         order: ['header', 'querystring', 'cookie'],
